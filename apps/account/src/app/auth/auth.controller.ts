a09213367a002086806d7c05be55d9f5dfbcdaeb
Sasha <ivanpolarus2009@gmail.com>
2023-08-23 08:45:42 +0300

    feat: add contracts

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,16 +1,6 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
-export class RegisterDto {
-	email: string;
-	password: string;
-	displayName?: string;
-}
-
-export class LoginDto {
-	email: string;
-	password: string;
-}
+import { AccountLogin, AccountRegister } from '@code/contracts';
 
 @Controller('auth')
 export class AuthController {
@@ -19,14 +9,14 @@ export class AuthController {
 	) {}
 
 	@Post('register')
-	async register(@Body() dto: RegisterDto) {
+	async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
 		const result = await this.authService.register(dto);
 		return result;
 	}
 
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: LoginDto) {
+	async login(@Body() dto: AccountLogin.Request): Promise<AccountLogin.Response> {
 		const { id } = await this.authService.validateUser(dto.email, dto.password);
 		const result = await this.authService.login(id);
 		return result;
